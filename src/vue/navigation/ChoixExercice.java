148f419e204aa578fedcad02686ad1a2070726d8
Anatole <anatole.gilet@ntymail.com>
2017-03-30 11:50:22 +0200

    JD choix exercice

--- src/vue/navigation/ChoixExercice.java
+++ src/vue/navigation/ChoixExercice.java
@@ -2,6 +2,19 @@ package vue.navigation;
 
 import javax.swing.JPanel;
 
+/**
+ * <b> Affichage du panel contenant les exercices.<b/>
+ * <p>
+ * Contient un JTable composé de JButton qui utilise le modèle ModelTableauExercice
+ * Renvoie vers le JFrame exercice
+ * Fonctionnalitées :
+ * <ul>
+ * <li>récupération des exercices via la classe de l'eleve</li>
+ * <li>exercices finis (tentative au statut == 2) ont un bouton désactivé</li>
+ * </ul>
+ * </p>
+ * @author Groupe 4
+ */
 public class ChoixExercice extends JPanel {
 
 }
