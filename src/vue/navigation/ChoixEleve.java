fb243befb4bf4dfd132f8484194fec773111efea
Anatole <anatole.gilet@ntymail.com>
2017-03-30 11:31:43 +0200

    JD choixeleve

--- src/vue/navigation/ChoixEleve.java
+++ src/vue/navigation/ChoixEleve.java
@@ -2,6 +2,15 @@ package vue.navigation;
 
 import javax.swing.JPanel;
 
+
+/**
+ * <b> Affichage du panel contenant les eleves pour qu'ils puissent entrer dans leur compte.<b/>
+ * <p>
+ * Contient un JTable avec modèle TableauEleve
+ * Renvoie vers le panel ChoixExercice une fois le profil 
+ * </p>
+ * @author Groupe 4
+ */
 public class ChoixEleve extends JPanel {
 
 }
