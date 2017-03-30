07b545b303d86d8f217ab6538b9af50eacf9a2af
Cailliaud <cailliaud.a@gmail.com>
2017-03-30 11:17:45 +0200

    commit des modeles tables et model tree

---
+++ src/vue/exercice/ClassTreeRenderer.java
@@ -0,0 +1,20 @@
+package vue.exercice;
+
+import java.awt.Component;
+
+import javax.swing.JTree;
+import javax.swing.tree.TreeCellRenderer;
+
+public class ClassTreeRenderer implements TreeCellRenderer {
+
+	@Override
+	public Component getTreeCellRendererComponent(JTree arg0, Object arg1, boolean arg2, boolean arg3, boolean arg4,
+			int arg5, boolean arg6) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+
+}
