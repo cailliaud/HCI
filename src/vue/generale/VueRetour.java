849d77aac714ad4582ad3edb241448dc33addad9
aarruebo <a.arruebo64@gmail.com>
2017-03-30 11:43:57 +0200

    modif package info et ajout de classe

---
+++ src/vue/generale/VueRetour.java
@@ -0,0 +1,13 @@
+package vue.generale;
+
+import javax.swing.JPanel;
+/**
+ * <p> Cette classe permet de naviguer sur des pages précédentes. </p> 
+ * <p> Elle est différente pour la page après la connexion car elle permet de se déconnecter.</p> 
+ * <p> Elle est également différence sur la page de connexion car elle permet de quitter l'application</p> 
+ * @author Groupe 4
+ *
+ */
+public class VueRetour extends JPanel {
+
+}
