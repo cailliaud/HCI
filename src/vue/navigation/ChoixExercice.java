39c566f5e40d84f20bdd06bdcc91ecdf8712ed05
aarruebo <a.arruebo64@gmail.com>
2017-03-30 12:11:35 +0200

    modif exercice et titre navigation

--- src/vue/navigation/ChoixExercice.java
+++ src/vue/navigation/ChoixExercice.java
@@ -1,7 +1,21 @@
 package vue.navigation;
 
 import javax.swing.JPanel;
-
+/** 
+ * <b> Cette classe permet à l'élève de choisir l'exercice qu'il souhaite effectué.</b> 
+ * 
+ * <p> Il sera représenté sous forme d'un table qui utulise la classe astraite ModelTableauExercice.</p> 
+ * <p> Il doit donc prendre en paramètre une liste d'exercice.</p> 
+ * <p> Pour lancer un exercice, l'élève doit appuyer sur le bouton contenu dans la ligne de l'exercice </p>
+ * <p> Lorsque l'élève n'a jamais réalier d'exercice, le résultat est automatiquement "???".</p> 
+ * <p> Lorsque l'élève a réalier un exercice, le résultat est modifier en fonction du résultat obtenu lors de 
+ * sa tentative. Lorsque la tentative n'est pas encore corrigée par le professeur, le statut est "en cours de correction"</p> 
+ * <p> Si le résultat est "acquis", l'exercice n'est plus disponible par l'élève, le bouton est grisée. </p>
+ * <p> Un élève ne peut pas lancer deux exercices en même temps </p>
+ * 
+ * @author lesap
+ *
+ */
 public class ChoixExercice extends JPanel {
 
 }
