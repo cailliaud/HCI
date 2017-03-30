9ef521ab8e81331eb8e961056ee4f2528f441214
Anatole <anatole.gilet@ntymail.com>
2017-03-30 12:26:25 +0200

    JD navigation

--- src/vue/navigation/Navigation.java
+++ src/vue/navigation/Navigation.java
@@ -2,6 +2,12 @@ package vue.navigation;
 
 import javax.swing.JFrame;
 
+/**
+ * <b> JFrame contenant tous les menus de navigation </b>
+ * <p>
+ * </p>
+ * @author Groupe 4
+ */
 public class Navigation extends JFrame {
 
 }
