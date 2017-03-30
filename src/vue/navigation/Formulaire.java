3f8c8c019d20730546446e2a7b9fce8cdbb0de9a
Cyp2C19 <cyprien8@hotmail.fr>
2017-03-30 12:09:12 +0200

    vues prof

--- src/vue/navigation/Formulaire.java
+++ src/vue/navigation/Formulaire.java
@@ -3,15 +3,18 @@ package vue.navigation;
 import javax.swing.JPanel;
 
 /**
- * <b>Vue correspondante au formulaire de connexion d'un professeur caractérisée par : 
+ * <b> Vue formulaire de connexion professeur</b>
+ * 
+ * Caractérisée par :
  * <ul>
- * 	   <li>L'affichage du nom de l'application
- *     <li>L'affichage d'un texte qui invite l'utilisateur à se connecter
- *     <li>Un champ login
- *     <li>Un champ mot de passe
+ * 	   <li>L'affichage du nom de l'application</li>
+ *     <li>L'affichage d'un texte qui invite l'utilisateur à se connecter</li>
+ *     <li>Un champ login</li>
+ *     <li>Un champ mot de passe</li>
  *     <li>Un bouton de connexion qui enverra l'utilisateur vers la vue MenuProf si les 
- *     	   informations de connexion sont correctes
- * <ul>
+ *     	   informations de connexion sont correctes</li>
+ *     <li>L'affichage d'un message d'erreur si les informations entrées ne sont pas valides</li>
+ * </ul>
  * 
  * @author Groupe 4
  *
---
+++ src/vue/navigation/MenuProf.java
@@ -0,0 +1,22 @@
+package vue.navigation;
+
+import javax.swing.JPanel;
+
+/**
+ * <b> Vue menu professeur permettant d'afficher au professeur authentifié des boutons de navigations</b>
+ * 
+ * Caractérisée par des boutons qui permettent d'orienter l'utilisateur vers : 
+ * <ul>
+ * 	   <li>La vue CreationExercice</li>
+ *     <li>La vue de visualisation de la classe d'un professeur</li>
+ *     <li>La vue de modification d'exercice</li>
+ *     <li>La vue de correction d'exercice</li>
+ * </ul>
+ * 
+ * @author Groupe 4
+ *
+ */
+
+public class MenuProf extends JPanel{
+
+}
