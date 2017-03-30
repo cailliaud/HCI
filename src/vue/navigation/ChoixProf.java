9c87edb5741842ea257a4c4a9b06f9a6b9cb597a
aarruebo <a.arruebo64@gmail.com>
2017-03-30 12:27:11 +0200

    modif choixProf

--- src/vue/navigation/ChoixProf.java
+++ src/vue/navigation/ChoixProf.java
@@ -1,7 +1,13 @@
 package vue.navigation;
 
 import javax.swing.JPanel;
-
+/**
+ *  <b> Cette classe permet de créer un tableau des différents professeur de l'école afin que les élèves sélectinne sa classe </b>
+ *  <p> Un professeur ne peut pas être associé à plus d'une classe. </p>
+ *  <p> Ce tableau est composé du nom du professeur et de son icone. </p>
+ * @author groupe 4
+ *
+ */
 public class ChoixProf extends JPanel {
 
 }
