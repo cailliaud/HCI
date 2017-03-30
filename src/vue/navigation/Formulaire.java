0ceeaeff6548289be850b2bb32820e61c0c4134a
Cyp2C19 <cyprien8@hotmail.fr>
2017-03-30 11:55:47 +0200

    Formulaire

--- src/vue/navigation/Formulaire.java
+++ src/vue/navigation/Formulaire.java
@@ -2,6 +2,21 @@ package vue.navigation;
 
 import javax.swing.JPanel;
 
+/**
+ * <b>Vue correspondante au formulaire de connexion d'un professeur caractérisée par : 
+ * <ul>
+ * 	   <li>L'affichage du nom de l'application
+ *     <li>L'affichage d'un texte qui invite l'utilisateur à se connecter
+ *     <li>Un champ login
+ *     <li>Un champ mot de passe
+ *     <li>Un bouton de connexion qui enverra l'utilisateur vers la vue MenuProf si les 
+ *     	   informations de connexion sont correctes
+ * <ul>
+ * 
+ * @author Groupe 4
+ *
+ */
+
 public class Formulaire extends JPanel {
 
 }
