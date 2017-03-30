47de4039c38be48939f9cdcf9be20f006b22ac87
Cailliaud <cailliaud.a@gmail.com>
2017-03-30 10:49:04 +0200

    ajout des classes JPanel

---
+++ src/vue/exercice/InfoTentative.java
@@ -0,0 +1,7 @@
+package vue.exercice;
+
+import javax.swing.JPanel;
+
+public class InfoTentative extends JPanel {
+
+}
---
+++ src/vue/exercice/MenuAction.java
@@ -0,0 +1,7 @@
+package vue.exercice;
+
+import javax.swing.JPanel;
+
+public class MenuAction extends JPanel {
+
+}
