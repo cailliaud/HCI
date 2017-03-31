85f6ba34e982b027604b2c059a191cedf08d3d88
Babujhi <babujhi.S@gmail.com>
2017-03-31 14:35:39 +0200

    Correction commentaire

--- src/vue/navigation/ChoixEleve.java
+++ src/vue/navigation/ChoixEleve.java
@@ -7,8 +7,10 @@ import javax.swing.JPanel;
  * <b> Affichage du panel contenant les eleves pour qu'ils puissent entrer dans leur compte.<b/>
  * <p>
  * Contient un JTable qui utilise le modèle ModelTableauEleve
- * Renvoie vers le panel ChoixExercice une fois le profil selectionne
+ * Renvoie vers le panel ChoixExercice une fois le profil selectionné.
+ * 
  * </p>
+ *
  * @author Groupe 4
  */
 public class ChoixEleve extends JPanel {
