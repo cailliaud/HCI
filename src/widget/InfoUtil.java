714761b9b089b33ebe4be731a19781cfdd573172
Cyp2C19 <cyprien8@hotmail.fr>
2017-04-05 15:12:44 +0200

    info util

--- src/widget/InfoUtil.java
+++ src/widget/InfoUtil.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 import modele.ecole.Utilisateur;
 
 public class InfoUtil extends JPanel{
@@ -15,7 +17,10 @@ public class InfoUtil extends JPanel{
 		
 		this.setLayout(new BorderLayout());
 		this.logo.setIcon(uti.getIcone());
+		this.logo.setHorizontalAlignment(SwingConstants.LEFT);
 		this.nomUtil.setText(uti.getPrenom());
+		this.nomUtil.setHorizontalAlignment(SwingConstants.CENTER);
+		
 		
 		this.add(logo, BorderLayout.WEST);
 		this.add(nomUtil, BorderLayout.CENTER);
