8bba9d2be2cc8cac04a1d4c318cd44a59ffbd46c
Cyp2C19 <cyprien8@hotmail.fr>
2017-04-05 15:20:00 +0200

    info util

--- src/widget/InfoUtil.java
+++ src/widget/InfoUtil.java
@@ -11,13 +11,13 @@ import modele.ecole.Utilisateur;
 public class InfoUtil extends JPanel{
 	
 	private JLabel logo = new JLabel();
-	private JLabel nomUtil;
+	private JLabel nomUtil = new JLabel();
 	
 	public InfoUtil(Utilisateur uti){
 		
 		this.setLayout(new BorderLayout());
 		this.logo.setIcon(uti.getIcone());
-		this.nomUtil = new JLabel(uti.getPrenom(), JLabel.CENTER);
+		this.nomUtil.setText(uti.getPrenom());
 		
 		this.add(logo, BorderLayout.WEST);
 		this.add(nomUtil, BorderLayout.CENTER);
