73b25ee90ff8e9925f32fb129efabeec26209eb9
Cyp2C19 <cyprien8@hotmail.fr>
2017-04-05 15:41:36 +0200

    infoUtil

--- src/widget/InfoUtil.java
+++ src/widget/InfoUtil.java
@@ -3,6 +3,7 @@ package widget;
 import java.awt.BorderLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -11,16 +12,16 @@ import modele.ecole.Utilisateur;
 
 public class InfoUtil extends JPanel{
 	
-	private JLabel logo = new JLabel();
+	private JButton logo = new JButton();
 	private JLabel nomUtil = new JLabel();
+	private Utilisateur uti;
 	
 	public InfoUtil(Utilisateur uti){
 		
+		this.uti = uti;
 		this.setLayout(new BorderLayout());
 		this.logo.setIcon(uti.getIcone());
 		this.nomUtil.setText(uti.getPrenom());
-		
-		this.logo.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		
 		this.add(logo, BorderLayout.WEST);
 		this.add(nomUtil, BorderLayout.CENTER);
